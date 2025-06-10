814f16947df14abf469d259156550c7585714e78
Michael07112002 <michaeljosephmcgrath7112@gmail.com>
2025-06-10 18:50:55 +0100

    first

---
+++ app.py
@@ -0,0 +1,188 @@
+
+# Import libraries
+import os
+
+from cs50 import SQL
+from flask import Flask, flash, redirect, render_template, request, session
+from flask_session import Session
+from werkzeug.security import check_password_hash, generate_password_hash
+from helpers import login_required
+import data_functions
+import pandas as pd 
+import sqlite3
+
+# Configure Application 
+app = Flask(__name__)
+
+# Configure session to use filesystem (instead of signed cookies)
+app.config["SESSION_PERMANENT"] = False 
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app) 
+
+# Connect to SQLite database and create the tables for it 
+db = SQL("sqlite:///FM.db") 
+
+# Create users table 
+
+db.execute("""CREATE TABLE IF NOT EXISTS users (
+           user_id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, 
+           username TEXT NOT NULL, 
+           hash TEXT NOT NULL
+           )
+           """)
+
+
+@app.after_request
+def after_request(response): 
+    """Ensure repsonses aren't cached"""
+    response.headers["Cache-Control"] = "no-cache, no-store, must revalidate"
+    response.headers["Expires"] = 0 
+    response.headers["Progma"] = "no-cache"
+    return response
+
+
+@app.route("/")
+@login_required
+def index():
+    return render_template("homepage.html")
+    
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    """Register User"""
+    if request.method == "POST": 
+        username = request.form.get("username")
+        password = request.form.get("password")
+        password_confirmation = request.form.get("password_confirmation")
+
+        # Validate user input 
+        if username and password and password_confirmation: 
+
+            # Check username is unique
+            check = db.execute("SELECT EXISTS(SELECT 1 FROM users WHERE username = ?) AS taken", username)
+            if check[0]["taken"] == 1: 
+                return redirect("/register") # Find way to also let user know the username is taken 
+            
+            # Check password and confirmation match 
+            if password == password_confirmation: 
+                
+                # Add user details to db 
+                db.execute("INSERT INTO users (username, hash) VALUES (?, ?)", username, generate_password_hash(password))
+
+                # Remember the user has logged in
+                session["user_id"] = db.execute("SELECT user_id FROM users WHERE username = ?", username)
+
+                # Redirect to homepage
+                return redirect("/")
+            
+            # User's password and confirmation do not match 
+            return redirect("/register") # Find way to let user know reason why 
+        
+        # User has failed to input fully 
+        return redirect("/register") # Find way to let user know reason why 
+    
+    # Request method is GET so allow user to enter page 
+    return render_template("register.html")
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login(): 
+    """Log user in"""
+
+    # Forget any user_id 
+    session.clear()
+
+    # User reached route via POST 
+    if request.method == "POST": 
+
+        print("POST request received")
+
+        # Ensure username and password was submitted 
+        if not request.form.get("username"): 
+            return redirect("/login") # Find way to let user know why 
+        if not request.form.get("password"): 
+            return redirect("/login") 
+        
+        # Query DB for username 
+        row = db.execute("SELECT * FROM users WHERE username = ?", request.form.get("username"))
+
+        # Ensure username exist and password is correct 
+        if len(row) != 1 or not check_password_hash(row[0]["hash"], request.form.get("password")): 
+            return redirect("/login") # Find way to let user know why 
+
+        # Remember which user has logged in 
+        session["user_id"] = row[0]["user_id"]
+
+        # Redirect user to homepage 
+        return redirect("/")
+    
+    # If request is GET allow user to access page
+    return render_template("login.html")
+    
+
+@app.route("/logout") 
+def logout(): 
+    """Log user out"""
+
+    # Forget user id 
+    session.clear() 
+
+    # Redirect user to login form 
+    return redirect("/") 
+
+
+@app.route("/create", methods=["GET", "POST"])
+@login_required
+def create(): 
+    if request.method == "POST":
+        # Take the csv files from the form submitted by the user
+        players_csv = request.files["players_csv"]
+        possession_csv = request.files["possession_csv"]
+
+        # Put the files in to dataframe and then clean and manipulate data within so it is ready to be used
+        df = data_functions.csv_file_cleaner_and_manipulator(players_csv, possession_csv)
+
+        # Add the data frame to the database and create user_id column to identify whose data is whose
+        df["user_id"] = session["user_id"]
+        conn = sqlite3.connect("FM.db")
+        df.to_sql("fm_data", conn, if_exists="replace", index=False)
+
+        return redirect("/player_search")
+
+    return render_template("create_data_set.html")
+
+@app.route("/player_search", methods=["GET", "POST"])
+@login_required 
+def player_search():
+    if request.method == "POST": 
+        conn = sqlite3.connect("FM.db")
+        df = pd.read_sql("SELECT * FROM fm_data", conn)
+        # Try and clean up by using list (DO NOT know if this will work though)
+        # parameters = ["uid", "wage", "transfer_value", "age", "matches", "percentage"]
+        # for parameter in parameters:
+        #     parameter = request.form.get(parameter)
+        uid = request.form.get("uid")
+        wage_input = request.form.get("wage")
+        wage = int(wage_input) if wage_input else None
+
+        transfer_value_input = request.form.get("transfer_value")
+        transfer_value = int(transfer_value_input) if transfer_value_input else None
+
+        age_input = request.form.get("age")
+        age = int(age_input) if age_input else None
+
+        matches_input = request.form.get("matches")
+        matches = int(matches_input) if matches_input else None
+
+        percentage_input = request.form.get("percentage") 
+        percentage = int(percentage_input) if percentage_input else None
+
+        df = data_functions.player_search(uid, df, wage, transfer_value, age, matches, percentage)
+        html_table = df.to_html(classes="table table-striped", index=False)
+
+        return render_template("player_search_results.html", table=html_table)
+    
+    return render_template("player_search.html")
+
+
+# Issues uploading csv file need to FIX
---
+++ data_functions.py
@@ -0,0 +1,312 @@
+import pandas as pd 
+import numpy as np 
+import re 
+
+#############################################################################################################
+# Remember the division column header has been loaded in as "Division_x" so this could cause bugs in future #
+#############################################################################################################
+
+# Read in csv file into DataFrames and create the pam df which we will use 
+data = pd.read_csv("/mnt/c/Users/Micha/Downloads/Data_Season4_Summer_Improved.csv", skiprows=1, header=0) 
+# data = data.iloc[1:].reset_index(drop=True)
+# data.columns = data.iloc[0]  # Use the first row as header
+
+
+poss = pd.read_csv("/mnt/c/Users/Micha/Downloads/Possession_Season4_Summer.csv", header=0) 
+poss.columns = poss.columns.str.strip()
+
+
+pam = pd.merge(data, poss, on = "Club")
+
+def csv_file_cleaner_and_manipulator(player_data_csv, possession_data_csv): 
+    player_data_df = pd.read_csv(player_data_csv, skiprows=1, header=0)
+    possession_data_df = pd.read_csv(possession_data_csv, header=0)
+    possession_data_df.columns = possession_data_df.columns.str.strip()
+    df = pd.merge(player_data_df, possession_data_df, on="Club")
+    age_to_int(df)
+    transfer_value_clean(df)
+    wage_clean(df)
+    distance_clean(df)
+    clean_percent_stats(df)
+    make_per_90(df)
+    possession_percent(df)
+    adjust_defensive(df)
+    adjust_offensive(df)
+    new_metrics(df)
+    position_group(df)
+    percentiles(df)
+    return df
+
+
+def age_to_int(df):
+    # Converts age value from string to int
+    df["Age"] = df["Age"].astype(int)
+
+
+def transfer_value_clean(df):
+    # Create new column for the cleaned transfer value data
+    df["Transfer Value Clean"] = 0.0
+    transfer_value_column = df.columns.get_loc("Transfer Value")
+    transfer_value_clean_column = df.columns.get_loc("Transfer Value Clean")
+
+    for i in range(len(df)): 
+        transfer_value = df.iloc[i, transfer_value_column]
+
+        # If transfer value is completley unknown and takes a string value then an extremely large value is attributed to it so that
+        # it is filtered to bottom when performing our player search
+        if transfer_value.strip() == "Not for Sale":
+            df.iloc[i, transfer_value_clean_column] = 100000000000
+    
+        elif transfer_value.strip() == "Unknown":
+            df.iloc[i, transfer_value_clean_column] = 0
+
+
+        # If the transfer value is given as a range between 2 values, the data is cleaned and then the mean of the 2 values is taken 
+        # and added to the new column
+        elif re.search(" - ", transfer_value): 
+            lower, upper = df.iloc[i, transfer_value_column].split(" - ") 
+            strings = [upper, lower]
+            for j in range(len(strings)): 
+                if re.search(r"^£\d+(\.\d+)?K$", strings[j]):
+                    strings[j] = float(strings[j].replace("£", "").replace("K", "")) * 1000
+                elif re.search(r"^£\d+(\.\d+)?M$", strings[j]):
+                    strings[j] = float(strings[j].replace("£", "").replace("M", "")) * 1000000
+                
+                # if transfer value is 0
+                else: 
+                    strings[j]= float(strings[j].replace("£", "")) 
+                
+            upper, lower = strings
+            mean_transfer_value = (float(upper) + float(lower))/2 
+            df.iloc[i, transfer_value_clean_column] = int(mean_transfer_value)
+
+        # If exact transfer value is known, it is simply just cleaned up and then added to the new column
+        else: 
+            if re.search(r"^£\d+(\.\d+)?K$", transfer_value):
+                    transfer_value = float(transfer_value.replace("£", "").replace("K", "")) * 1000
+            elif re.search(r"^£\d+(\.\d+)?M$", transfer_value):
+                transfer_value = float(transfer_value.replace("£", "").replace("M", "")) * 1000000
+            else: 
+                transfer_value = float(transfer_value.replace("£", "")) 
+            df.iloc[i, transfer_value_clean_column] = int(transfer_value)
+        
+
+def wage_clean(df):
+    # A new "wage clean" column is created and the new value for it is simply the integer value from the "wage" column
+    df["Wage Clean"] = df["Wage"].str.replace("£", "", regex = False)
+    df["Wage Clean"] = df["Wage Clean"].str.replace(",", "", regex = False)
+    df["Wage Clean"] = df["Wage Clean"].str.replace(" p/w", "", regex = False)
+    df["Wage Clean"] = df["Wage Clean"].fillna(0).astype(int)
+
+
+def possession_percent(df):
+    # The possession value from the possesion csv file is converted from a string value to a float decimal
+    df["Possession"] = (df["Possession"]).astype(float)/100 
+
+
+def distance_clean(df): 
+    df["Dist/90"] = df["Dist/90"].str.replace("km", "", regex=False)
+
+
+def clean_percent_stats(df): 
+    stats = ["Hdr %", "Tck R"]
+    for stat in stats: 
+            df[stat] = df[stat].replace("-", "0")
+            df[stat] = df[stat].str.replace("%", "", regex=False)
+            df[stat] = df[stat].astype(float)/100
+            
+
+
+def make_per_90(df): 
+    # The statistics that do not come already as "per 90" from FM are converted to it
+    stats = ["CCC", "xG-OP"]
+    for stat in stats: 
+        df[f"{stat}/90"] = df[stat]/90
+
+
+def adjust_defensive(df): 
+    # Defensive metrics are adjusted for possession
+    stats = ["K Tck/90", "Tck/90", "Blk/90", "Clr/90", "Int/90", "Pres A/90", "Pres C/90", "Poss Won/90", 
+             "Hdrs W/90", "K Hdrs/90", "Dist/90"]
+    for stat in stats: 
+        # Data is first cleaned and converted to float such that mathematical operations can be performed
+        df[stat] = df[stat].replace("-", 0)
+        df[stat] = df[stat].astype(float).round(2)
+        df[stat] = round(df[stat] / (1 - df["Possession"]) * 0.5, 2)
+        
+
+def adjust_offensive(df): 
+    # Offensive metrics are adjusted for possession
+    stats = ["Poss Lost/90", "K Ps/90", "CCC/90", "OP-KP/90", "Shot/90", "Pr passes/90", "xA/90", "Cr C/90", "Drb/90", 
+             "NP-xG/90", "xG-OP/90", "Ps A/90"] 
+    for stat in stats: 
+        # Data is first cleaned and converted to float such that mathematical operations can be performed
+        df[stat] = df[stat].replace("-", 0)
+        df[stat] = df[stat].astype(float).round(2)
+        df[stat] = round(df[stat] / df["Possession"] * 0.5, 2) 
+
+
+def new_metrics(df):  
+    # New metrics to be used are created from the existing ones
+    df["Poss Diff/90"] = df["Poss Won/90"] - df["Poss Lost/90"]
+    df["Def Act/90"] = df["Tck/90"] + df["Blk/90"] + df["Clr/90"] + df["Int/90"]
+    df["Pres %"] = df["Pres C/90"] / df["Pres A/90"] * 100
+    df["NP-xG per shot/90"] = df["NP-xG/90"] / df["Shot/90"]
+    df["(NP-xG + xA)/90"] = df["NP-xG/90"] + df["xA/90"]
+
+
+# Test what is done so far works on csv file
+def position_group(df): 
+    # Positions are grouped such that we can compare players in the same position groups to one another
+    position_groups = {
+        "GK": "Goalkeeper",
+        "D (C)": "Centre Back",
+        "D (R)": "Fullback",
+        "D (L)": "Fullback", 
+        "WB (R)": "Fullback", 
+        "WB (L)": "Fullback", 
+        "DM": "Defensive Midfielder", 
+        "M (C)": "Defensive Midfielder", 
+        "AM (C)": "Attacking Midfielder", 
+        "M (L)": "Attacking Midfielder", 
+        "M (R)": "Attacking Midfielder", 
+        "AM (L)": "Attacking Midfielder", 
+        "AM (R)": "Attacking Midfielder", 
+        "ST (C)": "Forward"
+    }
+    # A new column is created that takes the value of the corresponding position group to their best position as above
+    df["Position Group"] = df["Best Pos"].map(position_groups)
+
+
+
+def percentiles(df): 
+    # The percentile for each metric is calculated against players in the player's position group
+    stats = ["K Tck/90", "Tck/90", "Blk/90", "Clr/90", "Int/90", "Pres A/90", "Pres C/90", "Pres %", "Poss Won/90", 
+             "Hdrs W/90", "K Hdrs/90", "Poss Lost/90", "K Ps/90", "CCC/90", "OP-KP/90", "Shot/90", "Pr passes/90", "xA/90", 
+             "Cr C/90", "Drb/90", "NP-xG/90", "xG-OP/90", "Ps A/90", "Pas %", "Poss Diff/90", "Def Act/90", "NP-xG per shot/90", 
+             "(NP-xG + xA)/90", "Dist/90", "Tck R", "Hdr %"]
+    for stat in stats: 
+        df[f"{stat} Percentile"] = round(df.groupby("Position Group")[stat].rank(pct=True) * 100, 0)
+
+
+#df = csv_file_cleaner_and_manipulator("/mnt/c/Users/Micha/Downloads/Data_Season4_Summer_Improved.csv", "/mnt/c/Users/Micha/Downloads/Possession_Season4_Summer.csv")
+
+# Create the player search function
+position_group_stats = {
+    "Goalkeeper": ["xGP/90", "Sv %", "Poss Lost/90", "Poss Won/90", "Ps A/90", "Pas %", "Mins"], 
+
+    "Centre Back": ["K Tck/90", "Tck/90", "Tck R", "Def Act/90", "Hdr %", "Hdrs W/90", "K Hdrs/90", "Poss Diff/90", "Poss Lost/90", 
+                    "Poss Won/90", "Pr passes/90", "Pas %", "Ps A/90", "NP-xG/90", "Mins"],
+
+    "Fullback": ["Def Act/90", "Tck R", "Hdr %", "Hdrs W/90", "Poss Diff/90", "Poss Lost/90", 
+                    "Poss Won/90", "Pr passes/90", "Pas %", "Ps A/90", "(NP-xG + xA)/90", "Dist/90", "Drb/90", "CCC/90", "Shot/90",
+                    "OP-KP/90", "Tck R", "Pres A/90", "Pres %", "Cr C/90", "Mins"],
+
+    "Defensive Midfielder": ["Def Act/90", "Tck R", "Hdr %", "Hdrs W/90", "Poss Diff/90", "Poss Lost/90", 
+                    "Poss Won/90", "Pr passes/90", "Pas %", "Ps A/90", "(NP-xG + xA)/90", "Dist/90",
+                    "OP-KP/90", "Tck R", "Pres A/90", "Pres %", "Mins"], 
+
+    "Attacking Midfielder": ["Def Act/90", "Hdr %", "Poss Diff/90", "Poss Lost/90", 
+                    "Poss Won/90", "Pr passes/90", "Pas %", "Ps A/90", "NP-xG/90", "xA/90", "(NP-xG + xA)/90", "Dist/90", "Drb/90",
+                    "CCC/90", "Shot/90", "OP-KP/90", "Pres A/90", "Pres %", "Cr C/90", "Mins"],
+
+    "Forward": ["Def Act/90", "Hdr %", "Hdrs W/90", "Poss Diff/90", "Poss Lost/90", 
+                    "Poss Won/90", "Pas %", "Ps A/90", "NP-xG/90", "xA/90","(NP-xG + xA)/90", "Dist/90", "Drb/90", "CCC/90", "Shot/90",
+                    "OP-KP/90", "Tck R", "Pres A/90", "Pres %", "Mins"]
+}
+
+
+def player_search(player_uid, df, wage=None, transfer_value=None, age=None, matches=None, percentage=None):
+
+    position_group_stats = {
+                                "Goalkeeper": ["xGP/90", "Sv %", "Poss Lost/90", "Poss Won/90", "Ps A/90", "Pas %", "Mins"], 
+
+                                "Centre Back": ["K Tck/90", "Tck/90", "Tck R", "Def Act/90", "Hdr %", "Hdrs W/90", "K Hdrs/90",
+                                                 "Poss Diff/90", "Poss Lost/90", "Poss Won/90", "Pr passes/90", "Pas %", "Ps A/90",
+                                                   "NP-xG/90", "Mins"],
+
+                                "Fullback": ["Def Act/90", "Hdr %", "Hdrs W/90", "Poss Diff/90", "Poss Lost/90", 
+                                                "Poss Won/90", "Pr passes/90", "Pas %", "Ps A/90", "(NP-xG + xA)/90", "Dist/90", 
+                                                "Drb/90", "CCC/90", "Shot/90", "OP-KP/90", "Tck R", "Pres A/90", "Pres %", 
+                                                "Cr C/90", "Mins"],
+
+                                "Defensive Midfielder": ["Def Act/90", "Hdr %", "Hdrs W/90", "Poss Diff/90", "Poss Lost/90", 
+                                                "Poss Won/90", "Pr passes/90", "Pas %", "Ps A/90", "(NP-xG + xA)/90", "Dist/90",
+                                                "OP-KP/90", "Tck R", "Pres A/90", "Pres %", "Mins"], 
+
+                                "Attacking Midfielder": ["Def Act/90", "Hdr %", "Poss Diff/90", "Poss Lost/90", 
+                                                "Poss Won/90", "Pr passes/90", "Pas %", "Ps A/90", "NP-xG/90", "xA/90", 
+                                                "(NP-xG + xA)/90", "Dist/90", "Drb/90", "CCC/90", "Shot/90", "OP-KP/90", 
+                                                "Pres A/90", "Pres %", "Cr C/90", "Mins"],
+
+                                "Forward": ["Def Act/90", "Hdr %", "Hdrs W/90", "Poss Diff/90", "Poss Lost/90", 
+                                                "Poss Won/90", "Pas %", "Ps A/90", "NP-xG/90", "xA/90","(NP-xG + xA)/90", 
+                                                "Dist/90", "Drb/90", "CCC/90", "Shot/90", "OP-KP/90", "Tck R", "Pres A/90", 
+                                                "Pres %", "Mins"]
+                                }
+
+    # Find the player within the data frame using their UID
+    
+    player_stats_series = df.loc[df["UID"] == f"{player_uid}"]
+    
+    if player_stats_series.empty: 
+        raise KeyError("Player not found in data frame")
+    
+    # Set the optional parameters if they are not inputted
+    if wage == None: 
+        wage = player_stats_series["Wage Clean"].iloc[0]
+
+    if transfer_value == None: 
+        transfer_value = player_stats_series["Transfer Value Clean"].iloc[0]
+
+    if age == None: 
+        age = player_stats_series["Age"].iloc[0]
+
+    player_position = player_stats_series["Position Group"].iloc[0]
+    # Could add in position group to reduce the data frame earlier
+    df_reduced = df[(df["Wage Clean"] <= int(wage)) & (df["Transfer Value Clean"] <= int(transfer_value)) & (df["Age"] <= int(age)) 
+                    & (df["Position Group"] == player_position)]
+    
+    if matches == None:
+        matches = len(position_group_stats[player_position])
+
+    if percentage == None: 
+        percentage = 1
+
+    conditions = []
+
+    for stat in position_group_stats[player_position]: 
+        if stat == "Poss Lost/90":
+            conditions.append(df_reduced[stat] <= player_stats_series[stat].iloc[0] * percentage)
+        else: 
+            conditions.append(df_reduced[stat] >= player_stats_series[stat].iloc[0] * percentage)
+        
+    # Create a data frame that verifies each condition for the position
+    bool_df = pd.concat(conditions, axis=1)
+
+    # Count how many of the conditions are met 
+    matches_count = bool_df.sum(axis=1)
+
+    # create a data frame that includes the players that meet the required amount of conditions 
+    filtered_df = df_reduced[matches_count >= matches]
+
+    # Create columns list of the columns to be displayed to the user
+    columns = ["Name", "Position", "Division_x", "Club", "Age", "Transfer Value", "Wage"] + position_group_stats[player_position]
+    
+    return filtered_df[columns]
+        
+def show_percentiles(player_uid, df):
+    player_percentiles = df.loc[df["UID"] == f"{player_uid}"]
+    stats1 = ["(NP-xG + xA)/90", "Dist/90", "Drb/90", "CCC/90", "Tck R", "Drb/90", "CCC/90", "Tck R"]
+    stats2 = ["Pres A/90", "Pres %", "Cr C/90", "Shot/90", "OP-KP/90", "Shot/90", "OP-KP/90"]
+    print(player_percentiles[["Position Group", "K Tck/90 Percentile", "Hdrs W/90 Percentile", 
+                              "Poss Diff/90 Percentile", "Poss Lost/90 Percentile", "Poss Won/90 Percentile", 
+                              "Pr passes/90 Percentile"]])
+    print(player_percentiles[[f"{i} Percentile" for i in stats1]])
+    print(player_percentiles[[f"{i} Percentile" for i in stats2]])
+
+    
+df = csv_file_cleaner_and_manipulator("/mnt/c/Users/Micha/Downloads/Data_Season4_Summer_Improved.csv", 
+                                      "/mnt/c/Users/Micha/Downloads/Possession_Season4_Summer.csv")
+show_percentiles(2000025761, df)
+
